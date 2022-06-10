77459539170cad345625f63fb248c0db0c601745
gilotru <delacruzangelo686@gmail.com>
2022-06-10 18:10:37 +0800

    adding some content

--- script.js
+++ script.js
@@ -5,5 +5,33 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-    })
-})
+    });
+
+    //toggle menu/ nuvbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i').toggleClass("active")
+    });
+
+    //owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            100:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
